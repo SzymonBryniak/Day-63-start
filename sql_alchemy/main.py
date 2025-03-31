c479acecbff4ef10743b1fb345ab571bf0601e23
Szymon Bryniak <szymonbryniakproject@gmail.com>
2025-03-31 21:27:13 +0100

    Day 63 progress

--- sql_alchemy/main.py
+++ sql_alchemy/main.py
@@ -5,7 +5,7 @@ from sqlalchemy import Integer, String, Float
 
 
 class Base(DeclarativeBase):
-  pass
+ pass
 
 db = SQLAlchemy(model_class=Base)
 
@@ -13,6 +13,8 @@ db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids a warning
+
 # initialize the app with the extension
 db.init_app(app)
 
@@ -24,41 +26,43 @@ class User(db.Model):
     review: Mapped[float]
 
 
-with app.app_context():
-    db.create_all()
 
 
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run()
 
-@app.route("/users")
-def user_list():
-    users = db.session.execute(db.select(User).order_by(User.username)).scalars()
-    return render_template("user/list.html", users=users)
+# @app.route("/users")
+# def user_list():
+#     users = db.session.execute(db.select(User).order_by(User.username)).scalars()
+#     return render_template("user/list.html", users=users)
 
-@app.route("/users/create", methods=["GET", "POST"])
-def user_create():
-    if request.method == "POST":
-        user = User(
-            username=request.form["username"],
-            email=request.form["email"],
-        )
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for("user_detail", id=user.id))
+# @app.route("/users/create", methods=["GET", "POST"])
+# def user_create():
+#     if request.method == "POST":
+#         user = User(
+#             username=request.form["username"],
+#             email=request.form["email"],
+#         )
+#         db.session.add(user)
+#         db.session.commit()
+#         return redirect(url_for("user_detail", id=user.id))
 
-    return render_template("user/create.html")
+#     return render_template("user/create.html")
 
-@app.route("/user/<int:id>")
-def user_detail(id):
-    user = db.get_or_404(User, id)
-    return render_template("user/detail.html", user=user)
+# @app.route("/user/<int:id>")
+# def user_detail(id):
+#     user = db.get_or_404(User, id)
+#     return render_template("user/detail.html", user=user)
 
-@app.route("/user/<int:id>/delete", methods=["GET", "POST"])
-def user_delete(id):
-    user = db.get_or_404(User, id)
+# @app.route("/user/<int:id>/delete", methods=["GET", "POST"])
+# def user_delete(id):
+#     user = db.get_or_404(User, id)
 
-    if request.method == "POST":
-        db.session.delete(user)
-        db.session.commit()
-        return redirect(url_for("user_list"))
+#     if request.method == "POST":
+#         db.session.delete(user)
+#         db.session.commit()
+#         return redirect(url_for("user_list"))
 
-    return render_template("user/delete.html", user=user)
+#     return render_template("user/delete.html", user=user)
