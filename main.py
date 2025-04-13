7570795c618fa1366d27089f3240da02bf1bbb94
Szymon Bryniak <szymonbryniakproject@gmail.com>
2025-04-13 21:50:41 +0100

    Day 63 progress

--- main.py
+++ main.py
@@ -38,12 +38,8 @@ session = Session()
 # book = Books(id=3, title='Harry Potter',
 #                        author='J.K. Rowling',
 #                        review='9.3')
-
-
 with app.app_context():
     db.create_all()
-    
-    
 
 
 all_books = []
@@ -66,13 +62,21 @@ def add():
         #               "author": request.form.get("bauthor"),
         #               "rating": request.form.get("brating")})
         # print(all_books)
-        book = Books(id=3, title=request.form.get("bname"),
+        book = Books(title=request.form.get("bname"),
                        author=request.form.get("bauthor"),
                        review=request.form.get("brating"))
         print(book)
+
+
         with app.app_context():
             db.session.add(book)
             db.session.commit()
+
+
+    with app.app_context():
+         database = db.session.query(Books).all()
+         for i in database:
+            print(i.author)
 
 
     return render_template('add.html')
